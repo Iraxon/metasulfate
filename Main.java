36380210316de4f421da1975221349a5d22f26e8
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-02-15 12:06:29 -0500

    Expanded lexer test; applied automatic format

--- Main.java
+++ Main.java
@@ -5,36 +5,34 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void testSortLambda()
-    {
+    public static void testSortLambda() {
         List<String> testStrings = Arrays.asList(
-            new String[] {"abcde", "a", "abc", "ab", "abcd"}
-        );
+                new String[] { "abcde", "a", "abc", "ab", "abcd" });
         System.out.println("List:" + testStrings);
         testStrings.sort(
-            // Sort by String length, descending
-            (first, second) -> (second.length() - first.length())
-        );
+                // Sort by String length, descending
+                (first, second) -> (second.length() - first.length()));
         System.out.println("Sorted list:" + testStrings);
     }
-    public static void testMaxLambda()
-    {
+
+    public static void testMaxLambda() {
         Map<String, Integer> possible_matches = new HashMap<String, Integer>();
         possible_matches.put("ab", 2);
         possible_matches.put("abc", 42);
         possible_matches.put("abcde", 4);
         possible_matches.put("a", 102);
         String max = Collections.max(
-            possible_matches.keySet(),
-            (first, second) -> (first.length() - second.length())
-        );
+                possible_matches.keySet(),
+                (first, second) -> (first.length() - second.length()));
         System.out.println(max + ", " + possible_matches.get(max));
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         testMaxLambda();
-        System.out.println(Interpreter.lex(
-            "1 + 1"
-        ));
+
+        MetasulfateSession interpreter = new MetasulfateSession();
+
+        System.out.println(interpreter.lex(
+                "1 + 1 == myVariable_thing ** 4 + 5 (6 - 2) {34 + x}"));
     }
 }
