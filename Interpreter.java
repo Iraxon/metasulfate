99aff818101d6c6a2c3d951c8b685996e9bf3235
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-02-14 19:10:22 -0500

    Added semicolons to punctuation; moved public methods to top of class

--- Interpreter.java
+++ Interpreter.java
@@ -1,24 +1,95 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Map;
 import java.util.Set;
 
-record Token (
-    String type,
-    Object value
-) {}
+record Token (String type, Object value) {}
 
 public class Interpreter {
+
+    public static void run(String src)
+    /**
+     * Interprets raw Metasulfate source
+     */
+    {
+        List<Token> tokens = lex(src);
+        System.out.println(tokens);
+    }
+
+    public static void run_from_file(String path) throws FileNotFoundException
+    {
+        try {
+
+            String src = "";
+            Scanner s = new Scanner(new File(path));
+            while (s.hasNextLine()) {
+                src += s.nextLine();
+            }
+            s.close();
+
+            run(src);
+
+        } catch (FileNotFoundException e) {
+            throw e;
+        }
+    }
+
+    public static List<Token> lex(String src)
+    {
+
+        // We will return this thing's values:
+        List<Token> tokens = new ArrayList<Token>();
+
+        // For later:
+        Map<String, Token> possible_matches = new HashMap<String, Token>();
+        String builtString = "";
+        boolean foundWhitespace;
+
+        for (int i = 0; i < src.length(); i++) {
+            foundWhitespace = whitespace.contains(src.charAt(i));
+
+            if (foundWhitespace && possible_matches.size() > 0) {
+                    // Get the String key with the longest length
+                    String max = Collections.max(
+                        possible_matches.keySet(),
+                        (first, second) -> (first.length() - second.length())
+                    );
+                    tokens.add(possible_matches.get(max));
+                }
+            if (foundWhitespace || i + 1 == src.length()) {
+                builtString = "";
+                possible_matches.clear();
+            }
+            if (!foundWhitespace) {
+
+                builtString += src.substring(i, i + 1);
+                Token next = resolve(builtString);
+                if (next != null) {
+                    possible_matches.put(builtString, next);
+                }
+            }
+        }
+
+        return tokens;
+    }
+
+    final private static Set<Character> whitespace = new HashSet<Character>(
+            Arrays.asList(new Character[] {' ', '\n', '\t'})
+        );
 
     final private static Set<String> punctuation = new HashSet<String>(
         Arrays.asList(new String[] {
             "[", "]",
             "(", ")",
-            "{", "}"
+            "{", "}",
+            ";"
         })
     );
 
@@ -88,59 +159,5 @@ public class Interpreter {
             return new Token("int_lit", Integer.valueOf(s));
         }
         return resolveIdentifier(s);
-    }
-
-    public static List<Token> lex(String src)
-    {
-        final Set<Character> whitespace = new HashSet<Character>(
-            Arrays.asList(new Character[] {' ', '\n', '\t'})
-        );
-
-        // We will return this thing's values:
-        List<Token> tokens = new ArrayList<Token>();
-
-        // For later:
-        Map<String, Token> possible_matches = new HashMap<String, Token>();
-        String builtString = "";
-        boolean foundWhitespace;
-
-        for (int i = 0; i < src.length(); i++) {
-            foundWhitespace = whitespace.contains(src.charAt(i));
-
-            if (foundWhitespace && possible_matches.size() > 0) {
-                    // Get the String key with the longest length
-                    String max = Collections.max(
-                        possible_matches.keySet(),
-                        (first, second) -> (first.length() - second.length())
-                    );
-                    tokens.add(possible_matches.get(max));
-                }
-            if (foundWhitespace || i + 1 == src.length()) {
-                builtString = "";
-                possible_matches.clear();
-            }
-            if (!foundWhitespace) {
-
-                builtString += src.substring(i, i + 1);
-                Token next = resolve(builtString);
-                if (next != null) {
-                    possible_matches.put(builtString, next);
-                }
-            }
-        }
-
-        return tokens;
-    }
-    public static void run(String src)
-    /**
-     * Interprets raw Metasulfate source
-     */
-    {
-        ;
-    }
-
-    public static void run_from_file(String path)
-    {
-        ;
     }
 }
