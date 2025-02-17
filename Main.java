502a0ae51a6027f17dc302096fd8f92bb86b0b27
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-02-16 23:06:20 -0500

    Did work--still not done; need to fix math

--- Main.java
+++ Main.java
@@ -1,38 +1,9 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Main {
-    public static void testSortLambda() {
-        List<String> testStrings = Arrays.asList(
-                new String[] { "abcde", "a", "abc", "ab", "abcd" });
-        System.out.println("List:" + testStrings);
-        testStrings.sort(
-                // Sort by String length, descending
-                (first, second) -> (second.length() - first.length()));
-        System.out.println("Sorted list:" + testStrings);
-    }
-
-    public static void testMaxLambda() {
-        Map<String, Integer> possible_matches = new HashMap<String, Integer>();
-        possible_matches.put("ab", 2);
-        possible_matches.put("abc", 42);
-        possible_matches.put("abcde", 4);
-        possible_matches.put("a", 102);
-        String max = Collections.max(
-                possible_matches.keySet(),
-                (first, second) -> (first.length() - second.length()));
-        System.out.println(max + ", " + possible_matches.get(max));
-    }
-
     public static void main(String[] args) {
-        testMaxLambda();
 
-        MetasulfateSession interpreter = new MetasulfateSession();
+        MetasulfateSession interpreter = new MetasulfateSession(true);
 
-        System.out.println(interpreter.lex(
-                "1 + 1 == myVariable_thing ** 4 + 5 (6 - 2) {34 + x}"));
+        System.out.println(interpreter.eval(
+                "SUM SUM 1 3 'x"));
     }
 }
