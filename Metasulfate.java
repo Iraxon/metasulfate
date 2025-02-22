0b7d8fb3e763673afe48dc49efb091ac35dd3522
Iraxon <156947097+Iraxon@users.noreply.github.com>
2025-02-22 13:17:06 -0500

    WE HAVE A FUNCTIONAL INTERPRETER REJOICE

--- Metasulfate.java
+++ Metasulfate.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.LongBinaryOperator;
 
 public class Metasulfate {
-    public static void main(String[] args) {
-        System.out.println(
-                eval("LET 'double LAMBDA_DOT [PRODUCT DOT 2]. double 2"));
+    public static void main(final String[] args) {
+        System.out.println("\n\n\n---\n" +
+                eval("LET 'successor [SUM DOT 1] LET 'x 3 LET 'y 2 successor PROD 2 SUM x y") + "\n---\n\n\n");
     }
-    public static MesoValue eval(String src) {
-        return new Parser(lex(src)).parse(Scope.defaultScope);
+
+    public static MesoValue eval(final String src) {
+        return parse(lex(src));
     }
     // System.out.println("\n---\n" +
     // interpreter.evalFile("!standard_library.meso"));
@@ -38,21 +41,21 @@ public class Metasulfate {
     private static final Set<Character> whitespaceCompatible = new HashSet<>(
             Arrays.asList(new Character[] { '(', '\"' }));
 
-    private static List<String> lex(String rawSrc) {
+    private static List<String> lex(final String rawSrc) {
         final List<String> rVal = new ArrayList<>();
-        lexRec(rVal, rawSrc, "", 0);
+        _lex(rVal, rawSrc, "", 0);
         System.out.println("Lexer returning:\n" + rVal);
         return rVal;
     }
 
-    private static void lexRec(List<String> list, String src, String acc, int cursor) {
-        Consumer<String> add = (s) -> {
+    private static void _lex(final List<String> list, final String src, String acc, int cursor) {
+        final Consumer<String> add = (s) -> {
             if (s.length() > 0) {
                 list.add(s);
             }
         };
         char current;
-        int len = src.length();
+        final int len = src.length();
         for (cursor = 0; cursor < len; cursor++) {
             current = src.charAt(cursor);
             if (Character.isWhitespace(current)) {
@@ -64,8 +67,8 @@ public class Metasulfate {
                 }
             } else if (punctuation.contains(current)) {
                 add.accept(acc);
-                add.accept("" + current);
                 acc = "";
+                add.accept("" + current);
             } else {
                 if (cursor + 1 >= src.length()) {
                     add.accept(acc + current);
@@ -75,41 +78,55 @@ public class Metasulfate {
         }
     }
 
-    private static MesoValue parse(List<String> src) {
-        Parser parser = new Parser(src);
-        return parser.parse(Scope.defaultScope);
+    private static MesoValue parse(final List<String> src) {
+        return new Parser(src).parse(Scope.defaultScope);
     }
 }
 
 class Parser {
-    private List<String> src;
+    private final List<String> src;
     private int cursor;
 
-    public Parser(List<String> src) {
+    public Parser(final List<String> src) {
         this.src = src;
         this.cursor = 0;
     }
 
-    public MesoValue parse(Scope scope) {
-        MesoValue rVal = _parse(scope);
+    public MesoValue parse(final Scope scope) {
+        final MesoValue rVal = _parse(scope);
         if (hasNext()) {
             throw new IllegalArgumentException("Trailing data in program");
         }
         return rVal;
     }
 
-    private MesoValue _parse(Scope scope) {
-        String t = grab();
+    private MesoValue _parse(final Scope scope) {
+        final String t = grab();
+        try {
+            return new MesoInt(Long.valueOf(t));
+        } catch (final NumberFormatException e) {
+        }
+        if (MesoInt.opMap.containsKey(t)) {
+            return MesoInt.op(MesoInt.opMap.get(t), _parse(scope), _parse(scope));
+        }
         switch (t) {
-            case "LAMBDA_DOT":
+            case "[":
                 return new MesoClosure(grabDelimitedRange(t, "]"), scope);
             case "LET":
-                Scope letScope = scope.extend(_parse(scope), _parse(scope));
-                return _parse(letScope);
+                final MesoValue name = _parse(scope);
+                final MesoValue value = _parse(scope);
+                return _parse(scope.extend(name, value));
             case "\'":
                 return new MesoName(grab());
             default:
-                return scope.get(t);
+                MesoValue rVal = scope.get(t);
+                MesoValue arg;
+                while (rVal instanceof MesoClosure && (arg = _parse(scope)) != NoApply.NO_APPLY) {
+                    System.out.println("Applying: " + rVal);
+                    rVal = ((MesoClosure) rVal).apply(arg);
+                    System.out.println("rVal is now: " + rVal);
+                }
+                return rVal;
         }
     }
 
@@ -118,17 +135,9 @@ class Parser {
     }
 
     private String grab() {
-        String t = src.get(cursor++);
+        final String t = src.get(cursor++);
         System.out.println("Grabbing token: " + t);
         return t;
-    }
-
-    private List<String> grabN(int n) {
-        List<String> rval = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            rval.add(grab());
-        }
-        return rval;
     }
 
     /*
@@ -137,9 +146,9 @@ class Parser {
      * if the initiator is not null, then
      * count will be kept to allow for nesting
      */
-    private List<String> grabDelimitedRange(String initiator, String terminator) {
+    private List<String> grabDelimitedRange(final String initiator, final String terminator) {
         int nestingLevel = 1; // There was a starting opening brace
-        List<String> rval = new ArrayList<>();
+        final List<String> rval = new ArrayList<>();
         String current;
         while (true) {
             if (true) {
@@ -175,12 +184,32 @@ enum Quote implements MesoValue {
     QUOTE
 }
 
-record MesoName(String n) implements MesoValue {}
+record MesoInt(long v) implements MesoValue {
+    public static final Map<String, LongBinaryOperator> opMap = Map.ofEntries(
+        Map.entry("SUM", (x, y) -> (x + y)),
+        Map.entry("DIFF", (x, y) -> (x - y)),
+        Map.entry("DELTA", (x, y) -> (y - x)),
+        Map.entry("PROD", (x, y) -> (x * y)),
+        Map.entry("QUO", (x, y) -> (x / y)),
+        Map.entry("POW", (x, y) -> ((long) Math.pow(x, y)))
+    );
+
+    public static MesoInt op(final LongBinaryOperator op, final MesoValue x, final MesoValue y) {
+        return new MesoInt(op.applyAsLong(((MesoInt) x).v, ((MesoInt) y).v));
+    }
+
+    public String toString() {
+        return Long.toString(v) + " (int)";
+    }
+}
+
+record MesoName(String n) implements MesoValue {
+}
 
 record MesoClosure(List<String> def, Scope env) implements MesoValue {
 
-    public MesoValue apply(MesoValue arg) {
-        return new Parser(def).parse(env);
+    public MesoValue apply(final MesoValue arg) {
+        return new Parser(def).parse(env.extend("DOT", arg));
     }
 
     public String toString() {
@@ -194,19 +223,19 @@ record Scope(Scope outer, MesoName key, MesoValue value) {
             null, "T", MesoBool.T)
             .extend("F", MesoBool.F)
             .extend(".", NoApply.NO_APPLY);
-    public Scope(Scope outer, String k, MesoValue value) {
+    public Scope(final Scope outer, final String k, final MesoValue value) {
         this(outer, new MesoName(k), value);
     }
 
-    public Scope extend(MesoValue key, MesoValue value) {
-        return new Scope(this, ((MesoName)key), value);
+    public Scope extend(final MesoValue key, final MesoValue value) {
+        return new Scope(this, ((MesoName) key), value);
     }
 
-    public Scope extend(String key, MesoValue value) {
+    public Scope extend(final String key, final MesoValue value) {
         return new Scope(this, new MesoName(key), value);
     }
 
-    public MesoValue get(MesoValue k) {
+    public MesoValue get(final MesoValue k) {
         if (k.equals(key)) {
             return value;
         }
@@ -216,12 +245,12 @@ record Scope(Scope outer, MesoName key, MesoValue value) {
         throw new NoSuchElementException("Undefined name:\n" + k);
     }
 
-    public MesoValue get (String s) {
+    public MesoValue get(final String s) {
         return get(new MesoName(s));
     }
 
     public String toString() {
-        String out = "(Env: {";
+        String out = "(function, env: {";
         Scope current = this;
         while (current.outer != null && !current.equals(defaultScope)) {
             out += current.key + " == " + current.value + "; ";
